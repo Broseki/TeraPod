952d1096bb9ace8e56c28ce289d6b9c5ad52052e
Michael Canning <mcanning@cavefox.net>
2022-01-03 22:47:03 -0500

    Added comments to initial Database class definition

--- helpers/db.py
+++ helpers/db.py
@@ -1,3 +1,6 @@
+"""
+db.py: Contains all of the logic for interfacing with a MySQL/MariaDB database server
+"""
 import mysql.connector.pooling
 from mysql.connector.pooling import errors
 import time
@@ -18,10 +21,23 @@ cnxpool = mysql.connector.pooling.MySQLConnectionPool(pool_name="cnx_pool",
 
 
 class Database(object):
+    """
+    Database
+    Object holding a single database connection from the pool. This should be used with
+    the "with" context manager syntax.
+    """
     def __init__(self):
+        """
+        Initializer for Database objects. Does essentially nothing currently.
+        """
         return
 
     def __enter__(self):
+        """
+        Called when a context manager is entered. Pulls a connection off the pool.
+        If no connections are available this waits until one is available (blocking).
+        :return: The database object
+        """
         while True:
             try:
                 self.conn = cnxpool.get_connection()
@@ -31,5 +47,12 @@ class Database(object):
                 time.sleep(1)
 
     def __exit__(self, type_val, value, trace_back):
+        """
+        Closes the cursor and connection, returning the connection to the pool.
+        :param type_val: Type of exception (if any)
+        :param value: The value for the exception (if any)
+        :param trace_back: The exception traceback (if any)
+        :return: Nothing
+        """
         self.curr.close()
         self.conn.close()
